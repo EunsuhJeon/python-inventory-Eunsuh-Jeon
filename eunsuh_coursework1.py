351efad79996eb003c7f611b2f4b9a7fc0390ab1
Jade <lilyjun98@gmail.com>
2026-03-22 21:19:05 -0700

    Set up basic

---
+++ eunsuh_coursework1.py
@@ -0,0 +1,14 @@
+import os
+
+inventory = {}
+
+# sample data
+sample1 = {"name": "Laptop", "brand": "Dell", "category": "Electronics", "quantity": 5, "price": 799.99}
+sample2 = {"name": "Chair", "brand": "IKEA", "category": "Home", "quantity": 10, "price": 49.99}
+
+inventory[1] = sample_product_1
+inventory[2] = sample_product_2
+
+product_ids = {1, 2}
+next_id = 3
+
