3f392aa1d09d3cc1aa6edbb611743b2f1b1d46bf
Jade <lilyjun98@gmail.com>
2026-03-22 21:23:32 -0700

    Process user input and data

--- eunsuh_coursework1.py
+++ eunsuh_coursework1.py
@@ -12,3 +12,17 @@ inventory[2] = sample_product_2
 product_ids = {1, 2}
 next_id = 3
 
+def add_item():
+    global next_id
+    name = input("Enter the product name: ").strip()
+    category = input("Enter the category(Electronics, Home, Office): ").strip()
+    brand = input("Enter the brand name: ").strip()
+    quantity = int(input("Enter the quantity: ").strip())
+    price = float(input("Enter the price: ").strip())
+
+    item = {"name": name, "brand": brand, "category": category, "quantity": quantity, "price": price}
+    inventory[next_id] = item
+    product_ids.add(next_id)
+    print(f"\nID {next_id} item has been added!\n")
+    next_id += 1
+
